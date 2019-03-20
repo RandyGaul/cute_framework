65e4054fc31608ec9ed3582a8fe95d84f16139c5
RandyGaul <1919825+RandyGaul@users.noreply.github.com>
2019-03-19 23:38:37 -0700

    remove redundant cute_ from file types (wrote that on accident, by habit)

--- src/cute_file_system.h
+++ src/cute_file_system.h
@@ -27,7 +27,7 @@
 namespace cute
 {
 
-struct cute_file_t;
+struct file_t;
 
 enum file_type_t : int
 {
@@ -37,7 +37,7 @@ enum file_type_t : int
 	FILE_TYPE_OTHER,
 };
 
-struct cute_file_stat_t
+struct stat_t
 {
 	file_type_t type;
 	int is_read_only;
@@ -50,24 +50,24 @@ struct cute_file_stat_t
 extern CUTE_API int CUTE_CALL file_system_set_write_dir(const char* platform_dependent_directory);
 extern CUTE_API int CUTE_CALL file_system_mount(const char* archive, const char* mount_point, int append_to_path);
 extern CUTE_API int CUTE_CALL file_system_dismount(const char* archive);
-extern CUTE_API int CUTE_CALL file_system_stat(const char* virtual_path, cute_file_stat_t* stat);
-extern CUTE_API cute_file_t* CUTE_CALL file_system_create_file(const char* virtual_path);
-extern CUTE_API cute_file_t* CUTE_CALL file_system_open_file_for_write(const char* virtual_path);
-extern CUTE_API cute_file_t* CUTE_CALL file_system_open_file_for_append(const char* virtual_path);
-extern CUTE_API cute_file_t* CUTE_CALL file_system_open_file_for_read(const char* virtual_path);
-extern CUTE_API int CUTE_CALL file_system_close(cute_file_t* file);
+extern CUTE_API int CUTE_CALL file_system_stat(const char* virtual_path, stat_t* stat);
+extern CUTE_API file_t* CUTE_CALL file_system_create_file(const char* virtual_path);
+extern CUTE_API file_t* CUTE_CALL file_system_open_file_for_write(const char* virtual_path);
+extern CUTE_API file_t* CUTE_CALL file_system_open_file_for_append(const char* virtual_path);
+extern CUTE_API file_t* CUTE_CALL file_system_open_file_for_read(const char* virtual_path);
+extern CUTE_API int CUTE_CALL file_system_close(file_t* file);
 extern CUTE_API int CUTE_CALL file_system_delete(const char* virtual_path);
 extern CUTE_API int CUTE_CALL file_system_create_dir(const char* virtual_path);
 extern CUTE_API const char** CUTE_CALL file_system_enumerate_directory(const char* virtual_path);
 extern CUTE_API void CUTE_CALL file_system_free_enumerated_directory(const char** directory_list);
 extern CUTE_API int CUTE_CALL file_system_file_exists(const char* virtual_path);
-extern CUTE_API uint64_t CUTE_CALL file_system_read(cute_file_t* file, void* buffer, uint64_t bytes);
-extern CUTE_API uint64_t CUTE_CALL file_system_write(cute_file_t* file, const void* buffer, uint64_t bytes);
-extern CUTE_API int CUTE_CALL file_system_eof(cute_file_t* file);
-extern CUTE_API uint64_t CUTE_CALL file_system_tell(cute_file_t* file);
-extern CUTE_API int CUTE_CALL file_system_seek(cute_file_t* file, uint64_t position);
-extern CUTE_API uint64_t CUTE_CALL file_system_size(cute_file_t* file);
-extern CUTE_API int CUTE_CALL file_system_flush(cute_file_t* file);
+extern CUTE_API uint64_t CUTE_CALL file_system_read(file_t* file, void* buffer, uint64_t bytes);
+extern CUTE_API uint64_t CUTE_CALL file_system_write(file_t* file, const void* buffer, uint64_t bytes);
+extern CUTE_API int CUTE_CALL file_system_eof(file_t* file);
+extern CUTE_API uint64_t CUTE_CALL file_system_tell(file_t* file);
+extern CUTE_API int CUTE_CALL file_system_seek(file_t* file, uint64_t position);
+extern CUTE_API uint64_t CUTE_CALL file_system_size(file_t* file);
+extern CUTE_API int CUTE_CALL file_system_flush(file_t* file);
 extern CUTE_API int CUTE_CALL file_system_read_entire_file_to_memory(const char* virtual_path, void** data_ptr, uint64_t* size = NULL, void* user_allocator_context = NULL);
 extern CUTE_API int CUTE_CALL file_system_write_entire_buffer_to_file(const char* virtual_path, const void* data, uint64_t size);
 
