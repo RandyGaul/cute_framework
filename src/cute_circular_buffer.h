18a8f1bd87e67348cb8f689150275bc59d1a9af6
RandyGaul <1919825+RandyGaul@users.noreply.github.com>
2019-03-21 18:11:40 -0700

    Create cute_circular_buffer.h

---
+++ src/cute_circular_buffer.h
@@ -0,0 +1,49 @@
+/*
+	Cute Framework
+	Copyright (C) 2019 Randy Gaul https://randygaul.net
+
+	This software is provided 'as-is', without any express or implied
+	warranty.  In no event will the authors be held liable for any damages
+	arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
+
+	1. The origin of this software must not be misrepresented; you must not
+	   claim that you wrote the original software. If you use this software
+	   in a product, an acknowledgment in the product documentation would be
+	   appreciated but is not required.
+	2. Altered source versions must be plainly marked as such, and must not be
+	   misrepresented as being the original software.
+	3. This notice may not be removed or altered from any source distribution.
+*/
+
+#ifndef CUTE_CIRCULAR_BUFFER_H
+#define CUTE_CIRCULAR_BUFFER_H
+
+#include <cute_defines.h>
+
+namespace cute
+{
+
+struct circular_buffer_t
+{
+	int index0 = 0;
+	int index1 = 0;
+	int count = 0;
+	int capacity = 0;
+	void* data = NULL;
+	void* user_allocator_context = NULL;
+};
+
+extern CUTE_API circular_buffer_t CUTE_CALL circular_buffer_make(int initial_size_in_bytes, void* user_allocator_context = NULL);
+extern CUTE_API void CUTE_CALL circular_buffer_free(circular_buffer_t* buffer);
+
+extern CUTE_API int CUTE_CALL circular_buffer_push(circular_buffer_t* buffer, const void* data, int size);
+extern CUTE_API int CUTE_CALL circular_buffer_pull(circular_buffer_t* buffer, void* data, int size);
+extern CUTE_API void CUTE_CALL circular_buffer_clear(circular_buffer_t* buffer);
+
+}
+
+#endif // CUTE_CIRCULAR_BUFFER_H
