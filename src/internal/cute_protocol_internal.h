bf3352be392e556546ec07bf4d4976ee00a01fa0
RandyGaul <1919825+RandyGaul@users.noreply.github.com>
2019-04-07 01:01:12 -0700

    move things to protocol internal

--- src/internal/cute_protocol_internal.h
+++ src/internal/cute_protocol_internal.h
@@ -60,6 +60,79 @@ extern CUTE_API void CUTE_CALL packet_allocator_free(packet_allocator_t* packet_
 int packet_write(void* packet_ptr, packet_type_t packet_type, uint8_t* buffer, uint64_t game_id, uint64_t sequence, const crypto_key_t* key);
 void* packet_open(packet_allocator_t* pa, replay_buffer_t* nonce_buffer, uint64_t game_id, uint64_t timestamp, uint8_t* buffer, int size, uint64_t sequence_offset, const crypto_key_t* key, int is_server, packet_type_t* packet_type);
 
+
+
+struct packet_decrypted_connect_token_t
+{
+	uint64_t expire_timestamp;
+	uint64_t client_id;
+	uint64_t sequence_offset;
+	crypto_key_t key;
+	uint16_t endpoint_count;
+	endpoint_t endpoints[CUTE_CONNECT_TOKEN_SERVER_COUNT_MAX];
+	uint8_t user_data[CUTE_CONNECT_TOKEN_USER_DATA_SIZE];
+};
+
+struct packet_encrypted_connect_token_t
+{
+	uint64_t expiration_timestamp;
+	uint8_t nonce[CUTE_CONNECT_TOKEN_NONCE_SIZE];
+	uint8_t secret_data[CUTE_CONNECT_TOKEN_SECRET_SECTION_SIZE];
+};
+
+struct packet_connection_accepted_t
+{
+	int client_number;
+	int max_clients;
+};
+
+struct packet_connection_denied_t
+{
+	uint8_t packet_type;
+};
+
+struct packet_keepalive_t
+{
+	uint8_t packet_type;
+};
+
+struct packet_disconnect_t
+{
+	uint8_t packet_type;
+};
+
+struct packet_challenge_t
+{
+	uint64_t nonce;
+	uint8_t challenge_data[CUTE_CHALLENGE_DATA_SIZE];
+};
+
+struct packet_userdata_t
+{
+	int size;
+	uint8_t data[CUTE_PACKET_PAYLOAD_MAX];
+};
+
+struct connect_token_client_data_t
+{
+	uint64_t application_id;
+	uint64_t expiration_timestamp;
+	uint64_t creation_timestamp;
+	crypto_key_t client_to_server_key;
+	crypto_key_t server_to_client_key;
+	uint32_t handshake_timeout;
+	uint16_t endpoint_count;
+	endpoint_t endpoints[CUTE_CONNECT_TOKEN_SERVER_COUNT_MAX];
+};
+
+extern CUTE_API uint8_t* CUTE_CALL connect_token_process_client_data(uint8_t* buffer, connect_token_client_data_t* token);
+
+struct connect_token_t
+{
+	uint8_t nonce[CUTE_CONNECT_TOKEN_NONCE_SIZE];
+	uint8_t secret_data_and_hmac[CUTE_CONNECT_TOKEN_SECRET_SECTION_SIZE + CUTE_CRYPTO_HMAC_BYTES];
+};
+
 }
 
 #endif // CUTE_PROTOCOL_INTERNAL_H
