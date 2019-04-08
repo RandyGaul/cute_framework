ab43e4d07d58c20ef9e861032cccc80ee3611048
RandyGaul <1919825+RandyGaul@users.noreply.github.com>
2019-04-07 17:39:56 -0700

    working on encryption state from Cute Protocol standard next

--- test/main.cpp
+++ test/main.cpp
@@ -30,12 +30,13 @@
 
 #include <test_handle.h>
 #include <test_circular_buffer.h>
-#include <test_nonce_buffer.h>
+#include <test_replay_buffer.h>
 #include <test_crypto.h>
 #include <test_packet_queue.h>
 #include <test_socket.h>
 #include <test_connect_token.h>
 #include <test_packets.h>
+#include <test_encryption_state.h>
 
 int main(int argc, const char** argv)
 {
@@ -79,6 +80,7 @@ int main(int argc, const char** argv)
 		CUTE_TEST_CASE_ENTRY(test_packet_disconnect),
 		CUTE_TEST_CASE_ENTRY(test_packet_challenge),
 		CUTE_TEST_CASE_ENTRY(test_packet_payload),
+		CUTE_TEST_CASE_ENTRY(test_encryption_state_basic),
 	};
 	int test_count = sizeof(tests) / sizeof(*tests);
 	int fail_count = 0;
