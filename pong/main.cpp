1da6484c7f497ae8553694829e9c36a88a3a0c3e
apirux <apirux.gj@gmail.com>
2020-11-21 16:27:19 -0800

    Update main.cpp

--- pong/main.cpp
+++ pong/main.cpp
@@ -12,9 +12,8 @@ using namespace cute;
 int main(int argc, const char** argv)
 {
 	// Create a window with a resolution of 640 x 480, along with a DirectX 11 context.
-	uint32_t options = CUTE_APP_OPTIONS_D3D11_CONTEXT | CUTE_APP_OPTIONS_RESIZABLE | CUTE_APP_OPTIONS_WINDOW_POS_CENTERED;
-	app_t* app = app_make("CUTE PONG", 0, 0, 640, 480, options, argv[0]);
-	
+	app_t* app = app_make("CUTE PONG", 50, 50, 640, 480, CUTE_APP_OPTIONS_D3D11_CONTEXT, argv[0]);
+
 	while (app_is_running(app))
 	{
 		float dt = calc_dt();
