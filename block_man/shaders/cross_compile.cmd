a62c4314c84d8fd2c4a67c679650e893b9b9cc93
RandyGaul <1919825+RandyGaul@users.noreply.github.com>
2021-01-01 23:23:58 -0800

    remove block_man

--- block_man/shaders/cross_compile.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-for %%f in ("*.glsl") do call :compile %%~nf
-exit /B
-
-:compile
-@echo Compiling %~1.glsl into %~1_shader.h ...
-call "../../tools/sokol-shdc/win32/sokol-shdc.exe" --input %~1.glsl --output %~1_shader.h --slang glsl330:hlsl5:metal_macos:glsl300es:glsl100
