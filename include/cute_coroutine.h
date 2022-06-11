23478d249014c32c92a9425dfadb12a88b5c8668
Jon Stevens <jstevens@limyc.com>
2022-06-10 20:30:30 -0700

    RandyGaul/cute_framework#45 cute_coroutine.h - typedef structs, remove default arguments from C api, add back C++ api

--- include/cute_coroutine.h
+++ include/cute_coroutine.h
@@ -25,22 +25,22 @@
 #include "cute_defines.h"
 #include "cute_error.h"
 
-struct cf_coroutine_t;
+typedef struct cf_coroutine_t cf_coroutine_t;
 typedef void (cf_coroutine_fn)(cf_coroutine_t* co);
 
-CUTE_API cf_coroutine_t* CUTE_CALL cf_coroutine_make(cf_coroutine_fn* fn, int stack_size = 0, void* udata = NULL);
+CUTE_API cf_coroutine_t* CUTE_CALL cf_coroutine_make(cf_coroutine_fn* fn, int stack_size /*= 0*/, void* udata /*= NULL*/);
 CUTE_API void CUTE_CALL cf_coroutine_destroy(cf_coroutine_t* co);
 
-enum cf_coroutine_state_t
+typedef enum cf_coroutine_state_t
 {
 	CF_COROUTINE_STATE_DEAD,
 	CF_COROUTINE_STATE_ACTIVE_AND_RUNNING,
 	CF_COROUTINE_STATE_ACTIVE_BUT_RESUMED_ANOTHER,
 	CF_COROUTINE_STATE_SUSPENDED,
-};
+} cf_coroutine_state_t;
 
-CUTE_API cf_error_t CUTE_CALL cf_coroutine_resume(cf_coroutine_t* co, float dt = 0);
-CUTE_API float CUTE_CALL cf_coroutine_yield(cf_coroutine_t* co, cf_error_t* err = NULL);
+CUTE_API cf_error_t CUTE_CALL cf_coroutine_resume(cf_coroutine_t* co, float dt /*= 0*/);
+CUTE_API float CUTE_CALL cf_coroutine_yield(cf_coroutine_t* co, cf_error_t* err /*= NULL*/);
 CUTE_API cf_error_t CUTE_CALL cf_coroutine_wait(cf_coroutine_t* co, float seconds);
 CUTE_API cf_coroutine_state_t CUTE_CALL cf_coroutine_state(cf_coroutine_t* co);
 CUTE_API void* CUTE_CALL cf_coroutine_get_udata(cf_coroutine_t* co);
@@ -56,7 +56,25 @@ CUTE_API cf_coroutine_t* CUTE_CALL cf_coroutine_currently_running();
 
 namespace cute
 {
+using coroutine_t = cf_coroutine_t;
+using coroutine_fn = cf_coroutine_fn;
+using coroutine_state_t = cf_coroutine_state_t;
 
+CUTE_API coroutine_t* coroutine_make(coroutine_fn* fn, int stack_size = 0, void* udata = NULL) { return cf_coroutine_make(fn, stack_size, udata); }
+CUTE_API void coroutine_destroy(coroutine_t* co) { cf_coroutine_destroy(co); }
+
+CUTE_API error_t coroutine_resume(coroutine_t* co, float dt = 0) { return cf_coroutine_resume(co, dt = 0); }
+CUTE_API float coroutine_yield(coroutine_t* co, cf_error_t* err = NULL) { return cf_coroutine_yield(co, err); }
+CUTE_API error_t coroutine_wait(coroutine_t* co, float seconds) { return cf_coroutine_wait(co, seconds); }
+CUTE_API coroutine_state_t coroutine_state(coroutine_t* co) { return cf_coroutine_state(co); }
+CUTE_API void* coroutine_get_udata(coroutine_t* co) { return cf_coroutine_get_udata(co); }
+
+CUTE_API error_t coroutine_push(coroutine_t* co, const void* data, size_t size) { return cf_coroutine_push(co, data, size); }
+CUTE_API error_t coroutine_pop(coroutine_t* co, void* data, size_t size) { return cf_coroutine_pop(co, data, size); }
+CUTE_API size_t coroutine_bytes_pushed(coroutine_t* co) { return cf_coroutine_bytes_pushed(co); }
+CUTE_API size_t coroutine_space_remaining(coroutine_t* co) { return cf_coroutine_space_remaining(co); }
+
+CUTE_API coroutine_t* coroutine_currently_running() { return cf_coroutine_currently_running(); }
 }
 
 #endif // CUTE_CPP
