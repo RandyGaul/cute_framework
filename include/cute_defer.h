8072512881a38ff0b7898b434f5b84d2bba45f8c
Jon Stevens <jstevens@limyc.com>
2022-06-10 20:35:48 -0700

    RandyGaul/cute_framework#45 cute_defer.h - add namespaced type alias for cf_scope_exit<T>

--- include/cute_defer.h
+++ include/cute_defer.h
@@ -74,4 +74,9 @@ static cf_scope_exit<T> cf_s_create_scope_helper(T func)
  */
 #define CUTE_DEFER(L) const auto& CUTE_TOKEN_PASTE(cf_scope_exit, __LINE__) = cf_s_create_scope_helper([&]() { L; })
 
+namespace cute
+{
+template <typename T> using scope_exit = cf_scope_exit<T>;
+}
+
 #endif // CUTE_CPP
