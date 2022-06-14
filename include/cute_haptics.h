62b62d85e9a9dcb1085ba3ddf4c5a65833b4db3c
Jon Stevens <jstevens@limyc.com>
2022-06-13 22:16:04 -0700

    RandyGaul/cute_framework#45 cute_haptics.h - typedef structs,  add back C++ api

--- include/cute_haptics.h
+++ include/cute_haptics.h
@@ -38,7 +38,7 @@
  *
  * TODO - Open haptic on the device itself (e.g. for phones).
  */
-struct cf_haptic_t;
+typedef struct cf_haptic_t cf_haptic_t;
 
 /**
  * The envelope defines a fade in and fade out of strength for a haptic effect. The strength
@@ -64,21 +64,21 @@ struct cf_haptic_t;
  * From (1) to (4) is the effect duration (search for `duration_milliseconds` in this file).
  * From (3) to (4) is the `fade_milliseconds`.
  */
-struct cf_haptic_envelope_t
+typedef struct cf_haptic_envelope_t
 {
 	float attack;
 	int attack_milliseconds;
 	float fade;
 	int fade_milliseconds;
-};
+} cf_haptic_envelope_t;
 
-enum cf_haptic_type_t
+typedef enum cf_haptic_type_t
 {
 	CF_HAPTIC_TYPE_INVALID,
 	CF_HAPTIC_TYPE_LEFTRIGHT,
 	CF_HAPTIC_TYPE_PERIODIC,
 	CF_HAPTIC_TYPE_RAMP,
-};
+} cf_haptic_type_t;
 
 /**
  * The leftright haptic allows direct control of one larger and one smaller freqeuncy motors,
@@ -87,19 +87,19 @@ enum cf_haptic_type_t
  * `lo_motor_strength` must be from 0 to 1.
  * `hi_motor_strength` must be from 0 to 1.
  */
-struct cf_haptic_leftright_t
+typedef struct cf_haptic_leftright_t
 {
 	int duration_milliseconds;
 	float lo_motor_strength;
 	float hi_motor_strength;
-};
+} cf_haptic_leftright_t;
 
-enum cf_haptic_wave_type_t
+typedef enum cf_haptic_wave_type_t
 {
 	CF_HAPTIC_WAVE_TYPE_SINE,
 	CF_HAPTIC_WAVE_TYPE_TRIANGLE,
 	CF_HAPTIC_WAVE_TYPE_SAW,
-};
+} cf_haptic_wave_type_t;
 
 /**
  * A basic haptic for sine-based waveforms (https://en.wikipedia.org/wiki/Sine_wave).
@@ -107,7 +107,7 @@ enum cf_haptic_wave_type_t
  * `period` (time between each wave, or 1 / frequency).
  * `magnitude` (aka amplitude) must be from 0 to 1.
  */
-struct cf_haptic_periodic_t
+typedef struct cf_haptic_periodic_t
 {
 	cf_haptic_wave_type_t wave_type;
 	int duration_milliseconds;
@@ -115,7 +115,7 @@ struct cf_haptic_periodic_t
 	float magnitude;
 	/* phase - not included (not very useful) */
 	cf_haptic_envelope_t envelope;
-};
+} cf_haptic_periodic_t;
 
 /**
  * Adjusts the strength from `start` to `end` over `duration_milliseconds`.
@@ -123,15 +123,15 @@ struct cf_haptic_periodic_t
  * `start` must be from 0 to 1.
  * `end` must be from 0 to 1.
  */
-struct cf_haptic_ramp_t
+typedef struct cf_haptic_ramp_t
 {
 	int duration_milliseconds;
 	float start;
 	float end;
 	cf_haptic_envelope_t envelope;
-};
+} cf_haptic_ramp_t;
 
-struct cf_haptic_data_t
+typedef struct cf_haptic_data_t
 {
 	cf_haptic_type_t type;
 	union
@@ -140,11 +140,11 @@ struct cf_haptic_data_t
 		cf_haptic_periodic_t periodic;
 		cf_haptic_ramp_t ramp;
 	} u;
-};
+} cf_haptic_data_t;
 
 // -------------------------------------------------------------------------------------------------
 
-struct cf_joypad_t;
+typedef struct cf_joypad_t cf_joypad_t;
 
 /**
  * Attempts to open a joypad for haptics use.
@@ -169,7 +169,7 @@ CUTE_API void CUTE_CALL cf_haptic_set_gain(cf_haptic_t* haptic, float gain);
 /**
  * Represents a single instance of a `haptic_data_t` on a device.
  */
-struct cf_haptic_effect_t
+typedef struct cf_haptic_effect_t
 {
 	int id;
 
@@ -177,7 +177,7 @@ struct cf_haptic_effect_t
 	CUTE_INLINE bool is_valid();
 	#endif // CUTE_CPP
 
-};
+} cf_haptic_effect_t;
 
 CUTE_API CUTE_INLINE bool CUTE_CALL cf_haptic_effect_is_valid( const cf_haptic_effect_t* effect) { return effect->id > 0 ? true : false; }
 
@@ -241,17 +241,41 @@ CUTE_API void CUTE_CALL cf_haptic_rumble_play(cf_haptic_t* haptic, float strengt
  */
 CUTE_API void CUTE_CALL cf_haptic_rumble_stop(cf_haptic_t* haptic);
 
+
 #ifdef CUTE_CPP
 
 CUTE_INLINE bool cf_haptic_effect_t::is_valid() { return cf_haptic_effect_is_valid(this); }
 
-#endif // CUTE_CPP
-
-
-#ifdef CUTE_CPP
-
 namespace cute
 {
+
+using haptic_t = cf_haptic_t;
+using haptic_envelope_t = cf_haptic_envelope_t;
+using haptic_type_t = cf_haptic_type_t;
+using haptic_leftright_t = cf_haptic_leftright_t;
+using haptic_wave_type_t = cf_haptic_wave_type_t;
+using cf_haptic_periodic_t = cf_haptic_periodic_t;
+using haptic_ramp_t = cf_haptic_ramp_t;
+using haptic_data_t = cf_haptic_data_t;
+using joypad_t = cf_joypad_t;
+using haptic_effect_t = cf_haptic_effect_t;
+
+CUTE_INLINE haptic_t* haptic_open(joypad_t* joypad) { return cf_haptic_open(joypad); }
+CUTE_INLINE void haptic_close(haptic_t* haptic) { cf_haptic_close(haptic); }
+CUTE_INLINE bool haptic_supports(haptic_t* haptic, haptic_type_t type) { return cf_haptic_supports(haptic,type); }
+CUTE_INLINE void haptic_set_gain(haptic_t* haptic, float gain) { cf_haptic_set_gain(haptic,gain); }
+CUTE_INLINE bool haptic_effect_is_valid(const haptic_effect_t* effect) { return cf_haptic_effect_is_valid(effect); }
+CUTE_INLINE haptic_effect_t haptic_create_effect(haptic_t* haptic, haptic_data_t data) { return cf_haptic_create_effect(haptic,data); }
+CUTE_INLINE void haptic_run_effect(haptic_t* haptic, haptic_effect_t effect, int iterations) { cf_haptic_run_effect(haptic,effect,iterations); }
+CUTE_INLINE void haptic_update_effect(haptic_t* haptic, haptic_effect_t effect, haptic_data_t data) { cf_haptic_update_effect(haptic,effect,data); }
+CUTE_INLINE void haptic_stop_effect(haptic_t* haptic, haptic_effect_t effect) { cf_haptic_stop_effect(haptic,effect); }
+CUTE_INLINE void haptic_destroy_effect(haptic_t* haptic, haptic_effect_t effect) { cf_haptic_destroy_effect(haptic,effect); }
+CUTE_INLINE void haptic_pause(haptic_t* haptic) { cf_haptic_pause(haptic); }
+CUTE_INLINE void haptic_unpause(haptic_t* haptic) { cf_haptic_unpause(haptic); }
+CUTE_INLINE void haptic_stop_all(haptic_t* haptic) { cf_haptic_stop_all(haptic); }
+CUTE_INLINE bool haptic_rumble_supported(haptic_t* haptic) { cf_haptic_rumble_supported(haptic); }
+CUTE_INLINE void haptic_rumble_play(haptic_t* haptic, float strength, int duration_milliseconds) { cf_haptic_rumble_play(haptic,strength,duration_milliseconds); }
+CUTE_INLINE void haptic_rumble_stop(haptic_t* haptic) { cf_haptic_rumble_stop(haptic); }
 
 }
 
