5a31e018197580fc8e0de19266c7beb2817e2366
RandyGaul <randypgaul@gmail.com>
2023-03-03 20:02:18 -0800

    Documented cute_result.h

--- include/cute_result.h
+++ include/cute_result.h
@@ -42,22 +42,63 @@ enum
 	#undef CF_ENUM
 };
 
+/**
+ * @struct   CF_Result
+ * @category utility
+ * @brief    Information about the result of a function, containing any potential error details.
+ * @remarks  Check if a result is an error or not with `cf_is_error`.
+ * @related  CF_Result cf_is_error cf_result_make cf_result_error cf_result_success
+ */
 typedef struct CF_Result
 {
+	/* @member Either 0 for success, or -1 for failure. */
 	int code;
+
+	/* @member String containing details about any error encountered. */
 	const char* details;
 } CF_Result;
+// @end
 
+/**
+ * @function cf_is_error
+ * @category utility
+ * @brief    Returns true if a `CF_Result` contains an error.
+ * @param    result       The result.
+ * @related  CF_Result cf_is_error cf_result_make cf_result_error cf_result_success
+ */
 CUTE_INLINE bool cf_is_error(CF_Result result) { return result.code == CF_RESULT_ERROR; }
 
-CUTE_INLINE CF_Result cf_result_make(int code, const char* details) { CF_Result result; result.code = code; result.details = details; return result; }
+/**
+ * @function cf_result_error
+ * @category utility
+ * @brief    Returns a `CF_Result` containing an error.
+ * @param    details      Details about the error.
+ * @related  CF_Result cf_is_error cf_result_make cf_result_error cf_result_success
+ */
 CUTE_INLINE CF_Result cf_result_error(const char* details) { CF_Result result; result.code = CF_RESULT_ERROR; result.details = details; return result; }
+
+/**
+ * @function cf_result_success
+ * @category utility
+ * @brief    Returns a `CF_Result` as a success, containing no error information.
+ * @related  CF_Result cf_is_error cf_result_make cf_result_error cf_result_success
+ */
 CUTE_INLINE CF_Result cf_result_success() { CF_Result result; result.code = CF_RESULT_SUCCESS; result.details = NULL; return result; }
 
+/**
+ * @enum     CF_MessageBoxType
+ * @category utility
+ * @brief    Types of supported message boxes.
+ * @related  CF_MessageBoxType cf_message_box_type_to_string cf_message_box
+ */
 #define CF_MESSAGE_BOX_TYPE_DEFS \
-	CF_ENUM(MESSAGE_BOX_TYPE_ERROR, 0) \
-	CF_ENUM(MESSAGE_BOX_TYPE_WARNING, 1) \
-	CF_ENUM(MESSAGE_BOX_TYPE_INFORMATION, 2) \
+	/* @entry An error message box. */         \
+	CF_ENUM(MESSAGE_BOX_TYPE_ERROR, 0)         \
+	/* @entry A warning message box.. */       \
+	CF_ENUM(MESSAGE_BOX_TYPE_WARNING, 1)       \
+	/* @entry An informational message box. */ \
+	CF_ENUM(MESSAGE_BOX_TYPE_INFORMATION, 2)   \
+	/* @end */
 
 typedef enum CF_MessageBoxType
 {
@@ -66,6 +107,13 @@ typedef enum CF_MessageBoxType
 	#undef CF_ENUM
 } CF_MessageBoxType;
 
+/**
+ * @function cf_message_box_type_to_string
+ * @category utility
+ * @brief    Convert an enum `CF_MessageBoxType` to a c-style string.
+ * @param    state        The state to convert to a string.
+ * @related  CF_MessageBoxType cf_message_box_type_to_string cf_message_box
+ */
 CUTE_INLINE const char* cf_message_box_type_to_string(CF_MessageBoxType type)
 {
 	switch (type) {
@@ -76,6 +124,15 @@ CUTE_INLINE const char* cf_message_box_type_to_string(CF_MessageBoxType type)
 	}
 }
 
+/**
+ * @function cf_message_box
+ * @category utility
+ * @brief    Displays a message in a new window.
+ * @param    type       The type of the message box. See `CF_MessageBoxType`.
+ * @param    title      Title string of the window.
+ * @param    text       Text to display as the window's message.
+ * @related  CF_MessageBoxType cf_message_box_type_to_string cf_message_box
+ */
 CUTE_API void CUTE_CALL cf_message_box(CF_MessageBoxType type, const char* title, const char* text);
 
 #ifdef __cplusplus
@@ -116,7 +173,6 @@ CUTE_INLINE const char* to_string(MessageBoxType type)
 
 CUTE_INLINE bool is_error(Result error) { return cf_is_error(error); }
 
-CUTE_INLINE Result result_make(int code, const char* details) { return cf_result_make(code, details); }
 CUTE_INLINE Result result_failure(const char* details) { return cf_result_error(details); }
 CUTE_INLINE Result result_success() { return cf_result_success(); }
 CUTE_INLINE void message_box(MessageBoxType type, const char* title, const char* text) { return cf_message_box(type, title, text); }
--- src/cute_multithreading.cpp
+++ src/cute_multithreading.cpp
@@ -46,12 +46,18 @@ void cf_destroy_mutex(CF_Mutex* mutex)
 
 CF_Result cf_mutex_lock(CF_Mutex* mutex)
 {
-	return cf_result_make(cute_lock(mutex), NULL);
+	CF_Result result;
+	result.code = cute_lock(mutex);
+	result.details = NULL;
+	return result;
 }
 
 CF_Result cf_mutex_unlock(CF_Mutex* mutex)
 {
-	return cf_result_make(cute_unlock(mutex), NULL);
+	CF_Result result;
+	result.code = cute_unlock(mutex);
+	result.details = NULL;
+	return result;
 }
 
 bool cf_mutex_try_lock(CF_Mutex* mutex)
@@ -71,17 +77,26 @@ void cf_destroy_cv(CF_ConditionVariable* cv)
 
 CF_Result cf_cv_wake_all(CF_ConditionVariable* cv)
 {
-	return cf_result_make(cute_cv_wake_all(cv), NULL);
+	CF_Result result;
+	result.code = cute_cv_wake_all(cv);
+	result.details = NULL;
+	return result;
 }
 
 CF_Result cf_cv_wake_one(CF_ConditionVariable* cv)
 {
-	return cf_result_make(cute_cv_wake_one(cv), NULL);
+	CF_Result result;
+	result.code = cute_cv_wake_one(cv);
+	result.details = NULL;
+	return result;
 }
 
 CF_Result cf_cv_wait(CF_ConditionVariable* cv, CF_Mutex* mutex)
 {
-	return cf_result_make(cute_cv_wait(cv, mutex), NULL);
+	CF_Result result;
+	result.code = cute_cv_wait(cv, mutex);
+	result.details = NULL;
+	return result;
 }
 
 CF_Semaphore cf_make_sem(int initial_count)
@@ -96,22 +111,34 @@ void cf_destroy_sem(CF_Semaphore* semaphore)
 
 CF_Result cf_sem_post(CF_Semaphore* semaphore)
 {
-	return cf_result_make(cute_semaphore_post(semaphore), NULL);
+	CF_Result result;
+	result.code = cute_semaphore_post(semaphore);
+	result.details = NULL;
+	return result;
 }
 
 CF_Result cf_sem_try(CF_Semaphore* semaphore)
 {
-	return cf_result_make(cute_semaphore_try(semaphore), NULL);
+	CF_Result result;
+	result.code = cute_semaphore_try(semaphore);
+	result.details = NULL;
+	return result;
 }
 
 CF_Result cf_sem_wait(CF_Semaphore* semaphore)
 {
-	return cf_result_make(cute_semaphore_wait(semaphore), NULL);
+	CF_Result result;
+	result.code = cute_semaphore_wait(semaphore);
+	result.details = NULL;
+	return result;
 }
 
 CF_Result cf_sem_value(CF_Semaphore* semaphore)
 {
-	return cf_result_make(cute_semaphore_value(semaphore), NULL);
+	CF_Result result;
+	result.code = cute_semaphore_value(semaphore);
+	result.details = NULL;
+	return result;
 }
 
 CF_Thread* cf_thread_create(CF_ThreadFn func, const char* name, void* udata)
@@ -136,7 +163,10 @@ CF_ThreadId cf_thread_id()
 
 CF_Result cf_thread_wait(CF_Thread* thread)
 {
-	return cf_result_make(cute_thread_wait(thread), NULL);
+	CF_Result result;
+	result.code = cute_thread_wait(thread);
+	result.details = NULL;
+	return result;
 }
 
 int cf_core_count()
@@ -173,7 +203,10 @@ int cf_atomic_get(CF_AtomicInt* atomic)
 
 CF_Result cf_atomic_cas(CF_AtomicInt* atomic, int expected, int value)
 {
-	return cf_result_make(cute_atomic_cas(atomic, expected, value), NULL);
+	CF_Result result;
+	result.code = cute_atomic_cas(atomic, expected, value);
+	result.details = NULL;
+	return result;
 }
 
 void* cf_atomic_ptr_set(void** atomic, void* value)
@@ -188,7 +221,10 @@ void* cf_atomic_ptr_get(void** atomic)
 
 CF_Result cf_atomic_ptr_cas(void** atomic, void* expected, void* value)
 {
-	return cf_result_make(cute_atomic_ptr_cas(atomic, expected, value), NULL);
+	CF_Result result;
+	result.code = cute_atomic_ptr_cas(atomic, expected, value);
+	result.details = NULL;
+	return result;
 }
 
 CF_ReadWriteLock cf_make_rw_lock()
