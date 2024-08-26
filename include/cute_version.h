c9574611821fc8eb539d47df1db21a4066641218
RandyGaul <randypgaul@gmail.com>
2024-08-25 20:17:10 -0700

    Bump version to 1.1.0 (WIP)

--- include/cute_version.h
+++ include/cute_version.h
@@ -13,7 +13,7 @@
 //--------------------------------------------------------------------------------------------------
 // C API
 
-#define CF_VERSION_STRING_COMPILED "Cute Framework Version 1.0.2 (WIP)"
+#define CF_VERSION_STRING_COMPILED "Cute Framework Version 1.1.0 (WIP)"
 
 #ifdef __cplusplus
 extern "C" {
--- src/cute_version.cpp
+++ src/cute_version.cpp
@@ -10,5 +10,5 @@
 
 const char* cf_version_string_linked()
 {
-	return "Cute Framework Version 1.0.2 (WIP)";
+	return "Cute Framework Version 1.1.0 (WIP)";
 }
