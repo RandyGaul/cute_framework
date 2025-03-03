9444fdb453386cc7edf3c35716f2543e1550ec54
Piotr Usewicz <piotr@layer22.com>
2025-03-03 06:38:23 +0100

    Framerate functions (#259)
    * Add framerate functions
    
    * Add framerate implementation
    
    * Improve comment
    
    * Use lerping for smoothing
    
    I've increased the smoothing to the last 60 frames. This will cause the smoothed framerate to climb steadily in the first seconds of the running program.

--- src/cute_app.cpp
+++ src/cute_app.cpp
@@ -805,6 +805,22 @@ void cf_app_set_icon(const char* virtual_path_to_png)
 	cf_image_free(&img);
 }
 
+float cf_app_get_framerate()
+{
+	return 1.0f / CF_DELTA_TIME;
+}
+
+#ifndef CF_FRAMERATE_SMOOTHING
+#define CF_FRAMERATE_SMOOTHING 60.0f
+#endif
+
+float cf_app_get_smoothed_framerate()
+{
+	static float fps = 0;
+	fps = cf_lerp(fps, 1.0f / CF_DELTA_TIME, 1 / CF_FRAMERATE_SMOOTHING);
+	return fps;
+}
+
 ImGuiContext* cf_app_init_imgui()
 {
 	if (!app->gfx_enabled) return NULL;
