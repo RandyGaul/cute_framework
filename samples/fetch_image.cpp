134dfc8bb95683237e5dbe52a15a041dd65fe463
ogam <14947455+ogam@users.noreply.github.com>
2025-07-31 21:58:22 -0700

    removed debug bit

--- samples/fetch_image.cpp
+++ samples/fetch_image.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv[])
 {
-	int options = CF_APP_OPTIONS_WINDOW_POS_CENTERED_BIT | CF_APP_OPTIONS_RESIZABLE_BIT | CF_APP_OPTIONS_GFX_DEBUG_BIT;
+	int options = CF_APP_OPTIONS_WINDOW_POS_CENTERED_BIT | CF_APP_OPTIONS_RESIZABLE_BIT;
 	CF_Result result = cf_make_app("cute imgui image test", 0, 0, 0, 640, 480, options, argv[0]);
 	if (cf_is_error(result)) return -1;
 	
