b73e3e178efa03643be1a907a5b875c05aaa65aa
Randy Gaul <1919825+RandyGaul@users.noreply.github.com>
2026-02-16 16:29:31 -0800

    Export some more internal functions for clang linker on the unit tests

--- src/internal/cute_aseprite_cache_internal.h
+++ src/internal/cute_aseprite_cache_internal.h
@@ -22,8 +22,8 @@ CF_Result cf_aseprite_cache_load_from_memory(const char* unique_name, const void
 void cf_aseprite_cache_unload(const char* aseprite_path);
 CF_Result cf_aseprite_cache_load_ase(const char* aseprite_path, ase_t** ase);
 
-void cf_make_aseprite_cache();
-void cf_destroy_aseprite_cache();
+CF_API void cf_make_aseprite_cache();
+CF_API void cf_destroy_aseprite_cache();
 void cf_aseprite_cache_get_pixels(uint64_t image_id, void* buffer, int bytes_to_fill);
 
 // Per-aseprite asset. Indexed by asset_id in the flat array.
