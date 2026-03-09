4462b66b3ef6ea8e2a8a4bfa608eb2ba5d5a88de
Piotr Usewicz <piotr@layer22.com>
2026-03-09 05:11:32 +0100

    Do not force ccache (#491)
    This allows users who do not want to use ccache to... not use it.
    We set it in the GitHub build action already.

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -7,14 +7,5 @@ if (NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
   set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "RelWithDebInfo")
 endif ()
 
-find_program(CCACHE ccache)
-if (CCACHE)
-  message(STATUS "Using ccache")
-  set(CMAKE_C_COMPILER_LAUNCHER ${CCACHE})
-  set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
-else ()
-  message(STATUS "Ccache not found")
-endif ()
-
 # Generate compile_commands.json to make it easier to work with clang based tools
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
